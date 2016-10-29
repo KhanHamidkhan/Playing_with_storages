4db6d48547d662dd0923e8f9353b902f83df0d37
hamid khan <hamid khan>
2016-10-29 15:57:11 -0700

    Initial commit

---
+++ app/src/main/java/com/example/hamid_khan/playing_with_storages/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.hamid_khan.playing_with_storages;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
